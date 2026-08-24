c916a461b1b4a563b7b9d64d4e2a59767bdc11ce
Giulia004 <giuliafilicicchia@gmail.com>
2026-08-24 12:14:08 +0200

    Add docker

--- backend/demo/src/main/java/com/delivery/system/demo/security/JwtAuthFilter.java
+++ backend/demo/src/main/java/com/delivery/system/demo/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.lang.NonNull;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,7 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtAuthFilter extends OncePerRequestFilter{
+public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService userDetailsService;
 
@@ -25,7 +26,8 @@ public class JwtAuthFilter extends OncePerRequestFilter{
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException,IOException{
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+            FilterChain chain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
 
         if (header != null && header.startsWith("Bearer ")) {
